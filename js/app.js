4789cfce0abe7498d0dca05ea0f28a8e945801d4
TomaszAdamowicz <kontakt.adamowicz@gmail.com>
2017-08-08 18:39:59 +0200

    added some basic input logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation();
+
+var userPass = document.getElementById('login-user-pass');
+    btnSubmit = document.getElementById('login-user-pass-btn');
+    
+  btnSubmit.addEventListener('click', function(e){
+    console.log(userPass.value);
+  })
