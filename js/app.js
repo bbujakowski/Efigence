3d5f4a587f47f60eecb048c44aae9daed7ebd4d2
Bartosz Bujakowski <bbujakowski@efigence.com>
2017-08-11 13:54:58 +0200

    Adds code review messages

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 $(document).foundation();
 
 var userPass = document.getElementById('login-user-pass');
+// zapomniałeś tutaj dodac var, wiec zamiast klasycznej zmiennej zostanie utworzona
+// globalna zmienna (na obiekcie window), jeśli chcesz tworzyc kilka zmiennych jednym
+// wyrazem var, to linijka 3-cia powinna się kończyc przecinkiem a nie średnikiem
     btnSubmit = document.getElementById('login-user-pass-btn');
-    
+
+  // ucinamy zbędne rzeczy, w funkcji dodajesz parametr "e" ale
+  // z niego nie korzystasz, najlepiej uprzątac takie rzeczy
+  // + dziwne wcięcie =)
   btnSubmit.addEventListener('click', function(e){
     console.log(userPass.value);
   })
